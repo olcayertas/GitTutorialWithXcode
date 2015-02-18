e6d7a9126e6f5f167033f50b2fb9a759b4959bc5
Olcay Ertaş <olcayertas@gmail.com>
2015-02-18 10:24:22 +0200

    Initial Commit

---
+++ GitTutotrial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTutotrial
+//
+//  Created by Olcay Ertas on 18/02/15.
+//  Copyright (c) 2015 Humexis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitTutotrial/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GitTutotrial
+//
+//  Created by Olcay Ertas on 18/02/15.
+//  Copyright (c) 2015 Humexis. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
